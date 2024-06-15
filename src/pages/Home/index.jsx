740463922c0e97c48bf13ecc6e8d663e66ac0d5f
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2024-06-15 22:19:51 +0330

    fix charts

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Features from "../../components/Features";
+import "./index.css";
+import Chart from "../../components/Chart";
+import { xAxisData } from "../../datas";
 
 export default function Home() {
-  return <Features />;
+  return (
+    <div className="home">
+      <Features />
+      <Chart grid title={"Month Sale"} data={xAxisData} dataKey={"Sale"} />
+    </div>
+  );
 }
